59c906df386391a445a5d32d346c22ade57b428f
vcrfrancisco <vcrfrancisco@sga.pucminas.br>
2024-11-17 13:28:32 -0300

    chore: modularize login-user button logic

---
+++ baoba/src/js/user_menu_item.js
@@ -0,0 +1,13 @@
+const asyncLoginText = async () => {
+    const userMenuItem = document.getElementById("user-menu-item");
+
+    const userId = localStorage.getItem("user_id");
+
+    if (userId) {
+        userMenuItem.innerHTML = '<a href="./pages/meu-usuario.html">Meu Usuário</a>';
+    } else {
+        userMenuItem.innerHTML = '<a href="./pages/login.html">Login</a>';
+    }
+}
+
+asyncLoginText()
